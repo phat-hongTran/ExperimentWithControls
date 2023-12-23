61c9764f17e03267d17ceeed14fd3598dd3d51da
Phat Tran <ptran@strongtie.com>
2023-12-23 16:48:48 +0700

    Finish

--- ExperimentWithControls/MainWindow.xaml.cs
+++ ExperimentWithControls/MainWindow.xaml.cs
@@ -60,5 +60,21 @@ namespace ExperimentWithControls
                 number.Text = listBoxItem.Content.ToString();
             }
         }
+
+        private void readOnlyComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (readOnlyComboBox.SelectedItem is ListBoxItem listBoxItem)
+            {
+                number.Text = listBoxItem.Content.ToString();
+            }
+        }
+
+        private void editableComboBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (sender is ComboBox comboBox)
+            {
+                number.Text = comboBox.Text;
+            }
+        }
     }
 }
